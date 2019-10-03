65e61600c33c532281d53e1cf9a81e0aa01a9cc6
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-10-03 20:19:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,13 +14,16 @@ const returnLastTwoDrivers = function(driversArray) {
 
 let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
+
 let createFareMultiplier = function (integer) {
   return function (fare) {
     return fare * integer
   }
 }
 
+
 const fareDoubler = createFareMultiplier(2);
+
 
 const fareTripler = createFareMultiplier(3);
 
