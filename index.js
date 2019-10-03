92e977994e38e43f3b69c1ac1805c82ee4f770a8
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-10-03 20:15:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
+
 function fetchSpecifiedDrivers(driversArray, returnFunction) {
   return returnFunction(driversArray)
 }
